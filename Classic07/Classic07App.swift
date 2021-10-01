e2bafa5590167140673a3ed368538725d7aecb33
kb18k <jackexi@gmail.com>
2021-10-01 21:13:17 +0800

    Initial Commit

---
+++ Classic07/Classic07App.swift
@@ -0,0 +1,17 @@
+//
+//  Classic07App.swift
+//  Classic07
+//
+//  Created by kai wen chen on 2021/10/1.
+//
+
+import SwiftUI
+
+@main
+struct Classic07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Classic07/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Classic07
+//
+//  Created by kai wen chen on 2021/10/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
